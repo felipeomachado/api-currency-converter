2721a524a0bd726e1cbfa04999dc59f4f61f0174
Felipe Alexandre <felipeomachado@hotmail.com>
2020-12-02 17:38:46 -0300

    project created

---
+++ src/main/kotlin/br/com/felipeomachado/Hello.kt
@@ -0,0 +1,9 @@
+package br.com.felipeomachado
+
+import io.javalin.Javalin
+
+fun main(args: Array<String>) {
+    val app = Javalin.create().start(7000)
+    app.get("/") { ctx -> ctx.result("Hello Jaya") }
+}
+
