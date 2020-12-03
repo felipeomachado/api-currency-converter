8d8fb959157f2b9288288339ff867c17f48935d8
Felipe Alexandre <felipeomachado@hotmail.com>
2020-12-02 22:08:19 -0300

    gitignore fixed

--- src/test/kotlin/br/com/felipeomachado/api/ForeignExchangeRatesApiTest.kt
+++ src/test/kotlin/br/com/felipeomachado/api/ForeignExchangeRatesApiTest.kt
@@ -1,10 +1,20 @@
 package br.com.felipeomachado.api
 
+import org.junit.Rule
 import org.junit.Test
+import org.koin.test.KoinTest
+import org.koin.test.KoinTestRule
+import org.koin.test.inject
 import kotlin.test.assertTrue
 
-class ForeignExchangeRatesApiTest {
-    private val foreignExchangeRatesApi = ForeignExchangeRatesApi()
+
+class ForeignExchangeRatesApiTest : KoinTest {
+    private val foreignExchangeRatesApi by inject<ForeignExchangeRatesApi>()
+
+    @get:Rule
+    val koinTestRule = KoinTestRule.create {
+        modules(foreignExchangeRatesApiModuleModule)
+    }
 
     @Test
     fun `should get success on call the external api to request the rates when is a valid currency`() {
